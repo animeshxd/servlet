b04a14e49fb7487e257a36a4d6e2df17d471b534
Animesh Murmu <am2646374@gmail.com>
2022-06-21 19:23:24 +0530

    POST request with doPost()

---
+++ src/main/java/example/Add.java
@@ -0,0 +1,21 @@
+package example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Add extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int a = Integer.parseInt(req.getParameter("a"));
+        int b = Integer.parseInt(req.getParameter("b"));
+        PrintWriter out = resp.getWriter();
+        out.printf("%d + %d = %d", a, b , a + b);
+    }
+    
+}
