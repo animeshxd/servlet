9dba12f26b76f447da5cc02aea2872c7799e15e4
Animesh Murmu <am2646374@gmail.com>
2022-06-22 21:48:51 +0530

    ServletConfig with WebInitParam Annotation

--- src/main/java/example/A.java
+++ src/main/java/example/A.java
@@ -4,12 +4,17 @@ import java.io.IOException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "A", urlPatterns = "/a")
+@WebServlet(
+    name = "A", 
+    urlPatterns = "/a",
+    initParams = @WebInitParam(name = "data", value = "This data is only available for class A")
+)
 public class A extends HttpServlet {
 
     @Override
--- src/main/java/example/B.java
+++ src/main/java/example/B.java
@@ -4,12 +4,17 @@ import java.io.IOException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "B", urlPatterns = "/b")
+@WebServlet(
+    name = "B", 
+    urlPatterns = "/b",
+    initParams = @WebInitParam(name = "data", value = "This data is only available for class B")
+)
 public class B extends HttpServlet {
 
     @Override
