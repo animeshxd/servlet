6c18136d62715a83f0d6af077eb3489e2f7f33cf
Animesh Murmu <am2646374@gmail.com>
2022-06-22 11:35:23 +0530

    ServletContext - Globally persisted for every user

--- src/main/java/example/A.java
+++ src/main/java/example/A.java
@@ -2,11 +2,11 @@ package example;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class A extends HttpServlet {
 
@@ -15,10 +15,10 @@ public class A extends HttpServlet {
         int a = Integer.parseInt(req.getParameter("x"));
         int b = Integer.parseInt(req.getParameter("y"));
         int c = a + b;
-        HttpSession session = req.getSession();
-        session.setAttribute("a", a);
-        session.setAttribute("b", b);
-        session.setAttribute("c", c);
+        ServletContext context = req.getServletContext();
+        context.setAttribute("a", a);
+        context.setAttribute("b", b);
+        context.setAttribute("c", c);
         resp.sendRedirect("b");
     }
     
--- src/main/java/example/B.java
+++ src/main/java/example/B.java
@@ -2,21 +2,21 @@ package example;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class B extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        int a = (int) session.getAttribute("a");
-        int b = (int) session.getAttribute("b");
-        int c = (int) session.getAttribute("c");
-        resp.getWriter().println("This is class B: ");
+        ServletContext context = req.getServletContext();
+        int a = (int) context.getAttribute("a");
+        int b = (int) context.getAttribute("b");
+        int c = (int) context.getAttribute("c");
+        resp.getWriter().println("This is class B: using ServletContext");
         resp.getWriter().printf("%d + %d= %d", a, b, c);
 
 
