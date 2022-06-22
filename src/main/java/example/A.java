08814c8a7f36dabfa49e4ccc027bfb662b6bfb39
Animesh Murmu <am2646374@gmail.com>
2022-06-22 21:42:53 +0530

    WebServlet Annotation - servlet configuration with annotation

--- src/main/java/example/A.java
+++ src/main/java/example/A.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name = "A", urlPatterns = "/a")
 public class A extends HttpServlet {
 
     @Override
@@ -15,6 +17,8 @@ public class A extends HttpServlet {
         ServletConfig context = getServletConfig();
         String data = context.getInitParameter("data");
         resp.getWriter().println(data);
+        resp.getWriter().println("This is class A");
+
                
     }
     
--- src/main/java/example/B.java
+++ src/main/java/example/B.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name = "B", urlPatterns = "/b")
 public class B extends HttpServlet {
 
     @Override
@@ -15,6 +17,7 @@ public class B extends HttpServlet {
         ServletConfig context = getServletConfig();
         String data = context.getInitParameter("data");
         resp.getWriter().println(data);
+        resp.getWriter().println("This is class B");
     }
     
 }
